dad3c7a2457638f7aa8755e6cc0e6a3964431db2
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-05-28 19:58:58 +0300

    readme update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,12 @@ import { Component } from "react";
 
 class HomePage extends Component {
   render() {
-    console.log(isNaN("la"));
     return (
       <div className="homePage">
         <h1>Welcome to my store!</h1>
         <p>Find your next pair of shoes for every occasion.</p>
         <img src="https://images.pexels.com/photos/1630344/pexels-photo-1630344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shoes"></img>
+        <p style={{ fontSize: "14px", color: "  rgba(121, 201, 121, 0.419)", marginTop: "100px" }}> &copy;Liat Pardo Grinbaum&copy;</p>
       </div>
     );
   }
